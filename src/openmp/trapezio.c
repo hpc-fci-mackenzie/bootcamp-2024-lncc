fd69471e44c01e759b0f1fce803cb58d9672efaf
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2024-02-02 01:07:03 -0300

    Adicionando exemplo do trapezio

---
+++ src/openmp/trapezio.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+double f(double x)
+{
+  double valor;
+  valor = exp(x);
+  return (valor);
+}
+
+int main(int argc, char *argv[])
+{
+  double integral; /* Armazena resultado em integral */
+  double a, b;     /* Limite esquerdo e direito */
+  long n;          /* Número de Trapezóides */
+  double h;        /* Largura da base do Trapezóide */
+  a = 0.0;
+  b = 1.0;
+  n = 8000000000;
+  // retire o comentário abaixo para fazer a leitura do valor
+  // printf("Entre a, b, e n \n");
+  // scanf("%lf %lf %ld", &a, &b, &n);
+  h = (b - a) / n;
+  integral = (f(a) + f(b)) / 2.0;
+  double t_inicio = omp_get_wtime();
+
+#pragma omp parallel for default(none) firstprivate(n, a, h) reduction(+ : integral) schedule(static) num_threads(4)
+  for (long i = 1; i < n - 1; i++) {
+    integral += f(a + i * h);
+  }
+  integral *= h;
+
+  double t_fim = omp_get_wtime();
+  printf("Com n = %ld trapezoides, a estimativa \n", n);
+  printf("da integral de %f ate %f = %lf \n", a, b, integral);
+  printf("Tempo: \t %f com %d threads.\n", t_fim - t_inicio, omp_get_max_threads());
+  return (0);
+}
